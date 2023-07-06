86570c4a2dc0240fa14796619147f0309667080f
MaFlore <yawo.ahadjitse@ipnetinstitute.com>
2023-07-06 08:51:54 +0000

    Ajout ArticleServiceImpl

--- src/main/java/com/example/fileuploadspringboot/repository/ArticleRepository.java
+++ src/main/java/com/example/fileuploadspringboot/repository/ArticleRepository.java
@@ -2,6 +2,8 @@ package com.example.fileuploadspringboot.repository;
 
 import com.example.fileuploadspringboot.model.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 }
---
+++ src/main/java/com/example/fileuploadspringboot/serviceImpl/ArticleServiceImpl.java
@@ -0,0 +1,40 @@
+package com.example.fileuploadspringboot.serviceImpl;
+
+import com.example.fileuploadspringboot.model.Article;
+import com.example.fileuploadspringboot.repository.ArticleRepository;
+import com.example.fileuploadspringboot.service.ArticleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ArticleServiceImpl implements ArticleService {
+
+    @Autowired
+    private ArticleRepository articleRepository;
+    @Override
+    public List<Article> getAll() {
+        return articleRepository.findAll();
+    }
+
+    @Override
+    public Article findById(Long id) {
+        return articleRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Article add(Article article) {
+        return articleRepository.save(article);
+    }
+
+    @Override
+    public Article update(Article article) {
+        return articleRepository.save(article);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        articleRepository.deleteById(id);
+    }
+}
